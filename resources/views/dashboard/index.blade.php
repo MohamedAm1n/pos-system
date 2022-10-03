bdf13246b8e1285129b73f4080606ca7cedb4db1
mohamed am1n <mohamedamn534@gmail.com>
2022-10-03 14:35:09 +0200

    first

---
+++ resources/views/dashboard/index.blade.php
@@ -0,0 +1,4 @@
+@extends('layouts.dashboard.master')
+@section('content')
+<h1>hello</h1>
+@endsection
---
+++ resources/views/layouts/dashboard/master.blade.php
@@ -0,0 +1,15 @@
+
+@include('layouts.dashboard.head')
+@include('layouts.dashboard.nav')
+@include('layouts.dashboard.main_sidebar')
+
+<!-- Content Wrapper. Contains page content -->
+    <div class="content-wrapper">
+    <!-- Main content -->
+        <section class="content">
+            @yield('content')
+        </section>
+    <!-- /.content -->
+    </div>
+<!-- /.content-wrapper -->
+@include('layouts.dashboard.footer')
